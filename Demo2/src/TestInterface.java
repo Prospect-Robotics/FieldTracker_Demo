1eefcb8caa1bcc97d60096b7e60d699648bd21e9
adrianmgg <2adrian2@gmail.com>
2018-01-08 14:13:38 -0800

    aaa

---
+++ Demo2/src/TestInterface.java
@@ -0,0 +1,9 @@
+import amgg.FieldTracker;
+
+public class TestInterface {
+
+	public static void main(String[] args) {
+		FieldTracker tracker = new FieldTracker(0,0,0);
+	}
+
+}
---
+++ Demo2/src/amgg/FieldTracker.java
@@ -0,0 +1,66 @@
+package amgg;
+
+/**
+ * @author amgg
+ *
+ */
+public class FieldTracker {
+	double robotX;
+	double robotY;
+	double robotAngle;
+	
+	/**
+	 * @param robotX - starting x coordinate of robot
+	 * @param robotY - starting y coordinate of robot
+	 * @param robotAngle - starting angle of robot
+	 */
+	public FieldTracker(double robotX, double robotY, double robotAngle) {
+		this.robotX = robotX;
+		this.robotY = robotY;
+		this.robotAngle = robotAngle;
+	}
+
+	/**
+	 * @param angle - update the robot's angle in the tracker by this amount
+	 */
+	public void turnedRobot(double angle) {
+		robotAngle = FieldMath.wrapAngle(robotAngle + angle);
+	}
+
+	/**
+	 * @param distance - move the robot forwards in the tracker by this amount
+	 */
+	public void movedRobot(double distance) {
+		double sin = Math.sin(Math.toRadians(robotAngle));
+		double cos = Math.cos(Math.toRadians(robotAngle));
+		robotX += sin * distance;
+		robotY += cos * distance;
+	}
+
+	/**
+	 * @param x
+	 * @param y
+	 * @return - angle by which robot needs to turn
+	 */
+	public double getAngleToPoint(double x, double y) {
+		return FieldMath.angleToPoint(robotX, robotY, x, x, robotAngle);
+	}
+
+	/**
+	 * @param x
+	 * @param y
+	 * @return - distance by which to move robot
+	 */
+	public double getDistanceToPoint(double x, double y) {
+		return Math.sqrt(Math.pow((robotY - y), 2) + Math.pow((robotX - x), 2));
+	}
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public void navigateToPoint(double x,double y){
+		turnedRobot(getAngleToPoint(x,y));
+		movedRobot(getDistanceToPoint(x,y));
+	}
+}
