07abe38ecc6a1f797b9edcb860b04f6ecc0fb3da
adrianmgg <2adrian2@gmail.com>
2018-01-09 13:56:55 -0800

    created

---
+++ Demo2/src/amgg/PointMath.java
@@ -0,0 +1,27 @@
+package amgg;
+
+import java.awt.Point;
+
+class PointMath {
+	static final Point.Double ORIGIN = new Point.Double(0, 0);
+
+	static void rotate(Point.Double point, Point.Double center, double angle) {
+		double x1 = point.getX() - center.getX();
+		double y1 = point.getY() - center.getY();
+		double x2 = x1 * Math.cos(angle) - y1 * Math.sin(angle);
+		double y2 = x1 * Math.sin(angle) + y1 * Math.cos(angle);
+		x2 += center.getX();
+		y2 += center.getY();
+		point.setLocation(x2, y2);
+	}
+
+	static void move(Point.Double point, double x, double y) {
+		point.setLocation(point.getX() + x, point.getY() + y);
+	}
+
+	static void moveAlong(Point.Double point, double distance, double degrees) {
+		double x = point.getX() + (Math.sin(Math.toRadians(degrees) * distance));
+		double y = point.getY() + (Math.cos(Math.toRadians(degrees) * distance));
+		point.setLocation(x, y);
+	}	
+}
