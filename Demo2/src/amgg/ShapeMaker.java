d7f20af2b76dff93ee2c659a13efed45b9deba22
adrianmgg <2adrian2@gmail.com>
2018-01-09 15:32:14 -0800

    aa

--- Demo2/src/amgg/ShapeMaker.java
+++ Demo2/src/amgg/ShapeMaker.java
@@ -1,10 +1,29 @@
 package amgg;
 
+import java.awt.Point;
 import java.awt.Shape;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class ShapeMaker {
-	static Shape fromfile(File file){
+	static ArrayList<Shape> fromfile(File file) {
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		try {
+			Scanner in = new Scanner(file);
+			while(in.hasNext()){
+				ArrayList<Point.Double> pointslist = new ArrayList<Point.Double>();
+				String line = in.nextLine();
+				String[] points = line.split(" ");
+				for(String str:points){
+					String[] xy = str.split(",");
+					Point.Double point = new Point.Double(Double.valueOf(xy[0]),Double.valueOf(xy[1]));
+					pointslist.add(point);
+				}
+			}
+		} catch (FileNotFoundException e) {
+		}
 		return null;
 	}
 }
